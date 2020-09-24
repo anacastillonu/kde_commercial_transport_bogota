9e05c27ac180e8b0e0bd14e3b6e3b6904b310550
anacastillonu <ana.castillonu@gmail.com>
2020-09-24 15:43:57 -0500

    pyedits1

--- kde_dash.py
+++ kde_dash.py
@@ -1,1 +1,11 @@
+#-----------------------------------------------------------
+# IMPORT LIBRARIES
+#-----------------------------------------------------------
 
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+url = 'https://raw.githubusercontent.com/anacastillonu/kde_commercial_transport_bogota/master/data/kde.geojson'
+all_kde = gpd.read_file(url).set_crs(epsg=3116).to_crs(epsg=4326)
